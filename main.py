05b06e5b9545aaad3ac2a75f771dc72ede179a47
Aluko Ayomide <alukoayomide623@gmail.com>
2022-05-26 17:17:52 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,30 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
+
+    def change_name(self, name):
+        print(name)
+
+    def change_age(self, age):
+        print(age)
+    
+    def add_track(self, tracks):
+        print(tracks)
+
+    def get_score(self, score):
+        print(score)
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score(30.20)
+
+
+
